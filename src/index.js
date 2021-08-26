ef1a776ef092dcf5ed3e9614bd72f60cf5860873
Christian Fredricks <Christianpfred@gmail.com>
2021-08-26 11:43:05 -0500

    Added POST system has a few errors that can be fixed in merge or post merge

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+
+
+
+function* rootSaga() {
+yield takeEvery('ADD_FAVORITE', favoriteGif);
+}
+
+function* favoriteGif(action) {
+    console.log('bout to add a new favorite')
+    try {
+        const response = yield axios.post('/routes/favorite.router', action.payload);
+            console.log('POST /routes/favorite.router', response.data);
+    }
+    catch (err) {
+        console.log('favoriteGif has failed', error);
+        alert('favoriteGif was unable to add a favorite gif');
+    }
+};
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
