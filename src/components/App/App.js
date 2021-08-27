cb8cded8aa407c6319af9a8ebd74114274aa81d3
Dan St. Aubin <staubindj@gmail.com>
2021-08-27 10:57:26 -0500

    required commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Favorites from '../Favorites/Favorites.jsx';
 import Search from '../Search.js';
+import {HashRouter as Router, Route, Link } from 'react-router-dom';
 
 function App(props) {
 
   return (
     <div>
       <h1>Giphy Search!</h1>
-      <Favorites />
-      <Search />
+      <Router>
+      <header>
+        <ul>
+          <li><Link to="search">Search</Link></li>
+          <li><Link to="favorites">Favorites</Link></li>
+        </ul>
+      </header>
+        <Route path="/favorites" exact>
+          <Favorites />
+        </Route>
+        
+        <Route path="/search">
+          <Search />
+        </Route>
+
+      </Router>
     </div>
   )
   
